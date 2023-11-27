57f8f0ee368ee90ebd5ee4f5fa622988fb27369e
Himanshu Rawat <himanshurawat703@gmail.com>
2023-11-27 14:45:30 +0530

    ok

--- index.js
+++ index.js
@@ -9,14 +9,14 @@ global.foodData = require('./db')(function call(err, data, CatData) {
 const express = require('express')
 const app = express()
 const port = 5000
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 app.use(express.json())
 
 app.get('/', (req, res) => {
